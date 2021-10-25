508f45de9c2b465ac5959d8a28cefcfbb804d9e2
Sachin.B.Lohar <69034411+sachinl0har@users.noreply.github.com>
2021-10-25 11:53:35 +0530

    Add project files.

---
+++ Blog/Models/SeedData.cs
@@ -0,0 +1,31 @@
+﻿using Blog.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new BlogContext(
+                serviceProvider.GetRequiredService<DbContextOptions<BlogContext>>()))
+            {
+                // Look for any Blog Post
+                if (context.Post.Any())
+                {
+                    return;
+                }
+                context.Post.AddRange(
+                    new Post
+                    {
+                        Title = "Test Post",
+                        Description = "This is just a Test Post Ignore it.",
+                        Footer = "Happy Coding",
+                        CreatedAt = DateTime.Now
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
